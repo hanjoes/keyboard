d0b5fbf637ce42e4d087e4af5cef44fa77d8e03f
Hanzhou Shi <hanzhou87@gmail.com>
2018-03-28 23:08:05 -0700

    Minimal code to get a simple keyboard working.

--- _run/run.go
+++ _run/run.go
@@ -8,16 +8,18 @@ import (
 
 func main() {
 	kb := keyboard.NewKeyboard(false)
+	defer kb.Shutdown()
 	go func() {
 		kb.Start()
-		defer kb.Shutdown()
 	}()
 
 	for {
 		select {
 		case input := <-kb.In:
-			fmt.Printf("keyboard input: %q\n", input)
-			break
+			fmt.Printf("keyboard input: %v\n", input.Input)
+			if len(input.Input) == 1 && input.Input[0] == 3 {
+				return
+			}
 		}
 	}
 }
